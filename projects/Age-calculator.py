ff363dc6014b0134f49cc8d6de4125b75c8a7d37
Pravanit <sillypinkalt@gmail.com>
2026-07-10 18:47:31 +0530

    Implement age calculation loop in Age-calculator.py
    Added a loop to allow multiple age calculations until the user decides to exit.

--- projects/Age-calculator.py
+++ projects/Age-calculator.py
@@ -1,13 +1,23 @@
-#tryna do some project for my guthub let's see from datetime import datetime
+#tryna do some project for my guthub let's see
+from datetime import datetime
 
-print("Find 🙌 your age precisely by your birth date: ")
-print("===============================================")
-birth_year = int(input("Enter your birth year (e.g., 1990): "))
-birth_month = int(input("Enter your birth month (1-12): "))
-birth_day = int(input("Enter your birth day (1-31): "))
+# Loops starts
 
-current_date = datetime.now()
-birth_date = datetime(birth_year, birth_month, birth_day)
+while True:
+    print("Find 🙌 your age precisely by your birth date: ")
+    print("===============================================")
+    birth_year = int(input("Enter your birth year (e.g., 1990): "))
+    birth_month = int(input("Enter your birth month (1-12): "))
+    birth_day = int(input("Enter your birth day (1-31): "))
 
-age = current_date.year - birth_date.year - ((current_date.month, current_date.day) < (birth_month, birth_day))
-print("Your age is: ", age)
+    current_date = datetime.now()
+    birth_date = datetime(birth_year, birth_month, birth_day)
+
+    age = current_date.year - birth_date.year - ((current_date.month, current_date.day) < (birth_month, birth_day))
+    print("Your age is: ", age)
+    
+    #Exit statement
+    again = input("Do you want to calculate age again? (yes/no): ").lower()
+
+    if again != "yes":
+        break
