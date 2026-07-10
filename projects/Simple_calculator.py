fcb09b82e710a3add8a6f59aa56a250ce1d98937
Pravanit <sillypinkalt@gmail.com>
2026-07-10 21:05:32 +0530

    Refactor calculator to support repeated calculations
    Added a loop to allow repeated calculations and improved user prompts.

--- projects/Simple_calculator.py
+++ projects/Simple_calculator.py
@@ -1,32 +1,38 @@
-# Simple Calculator
+# Simple Calculator upon choice of two 
 
-# taking inputs
-num_1 = int(input("Enter your First number: "))
-num_2 = int(input("Enter your Second number: "))
-operation = input("Enter your operators from (/ * + - % **): ")
+# Loop  
+
+while True:
+    # taking inputs
+    num_1 = int(input("Enter your First number: "))
+    operation = input("Enter your operators from (/ * + - % **): ")
+    num_2 = int(input("Enter your Second number: "))
 
 
-# Conditional Statements
-if(operation == "+"):
-    print("The sum of your numbers is: ",num_1+num_2)
-elif(operation == "-"):
-    print("The difference of your numbersis: ",num_1-num_2)
-elif(operation == "/"):
-    if(num_2 == 0):
-        print("Error: Division by zero not allowed")    
+    # Conditional Statements
+    if(operation == "+"):
+        print("The sum of your numbers is: ",num_1+num_2)
+    elif(operation == "-"):
+        print("The difference of your numbersis: ",num_1-num_2)
+    elif(operation == "/"):
+        if(num_2 == 0):
+            print("Error: Division by zero not allowed")    
+        else:
+            print("The quotient of the Numbers is: ", num_1/num_2)
+    elif(operation == "*"):
+        print("The product of the numbers is: ",num_1*num_2)
+    elif(operation == "**"):
+        print("The power / exponent of number is: ", num_1**num_2)
+    elif(operation == "%"):
+        if num_2 == 0:
+            print("Error: Modulus by zero not allowed")
+        else:
+            print("The remainder of your numbers is:", num_1 % num_2)
     else:
-        print("The quotient of the Numbers is: ", num_1/num_2)
-elif(operation == "*"):
-    print("The product of the numbers is: ",num_1*num_2)
-elif(operation == "**"):
-    print("The power / exponent of number is: ", num_1**num_2)
-elif(operation == "%"):
-    if num_2 == 0:
-        print("Error: Modulus by zero not allowed")
-    else:
-        print("The remainder of your numbers is:", num_1 % num_2)
-else:
-    print("Invalid operator ")
-
+        print("Invalid operator ")
+     # Loops Exit 
+    again = input("Want to Calculat again? (yes/no): ").lower()
+    if again != "yes":
+        break
 
     
