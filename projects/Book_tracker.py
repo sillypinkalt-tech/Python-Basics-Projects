b1e86daa6ca21b59eb74ad8a3b5526954d3c0500
Pravanit <sillypinkalt@gmail.com>
2026-07-10 19:22:50 +0530

    Refactor book tracker to use loops for input handling
    Refactored book tracking logic to use a loop for continuous borrowing and returning. Updated book status messages and added author labels in the library dictionary.

--- projects/Book_tracker.py
+++ projects/Book_tracker.py
@@ -2,46 +2,38 @@
 # Checks if the book is available or borrowed 
 
 library = {
-    "HarryPotter": [ "J.K.Rowling","Available"], #<----- lists in dict 
-    "PercyJackson": ["RickRiordan","Available"],
-    "SherlockHolmes": [" ArthurConanDoyle","Available"]
+    "HarryPotter": [ " Author: J.K.Rowling","Available"], #<----- lists in dict 
+    "PercyJackson": [" Author: RickRiordan","Available"],
+    "SherlockHolmes": [" Author: ArthurConanDoyle","Available"]
 }
-books = [" HarryPotter: J.K.Rowling","PercyJackson: RickRiordan","SherlockHolmes: ArthurConanDoyle"]
-print("Books: ",books)
 
-#Taking inputs
-book = input("Enter Name of the book: ")
-status = input("Enter wether if you wanna Borrow / Return: ")
+# Loops
 
-#conditionals on basis of borrow or return
-if(status == "Borrow"):
-    if(library[book][1] == "Available"):
-      library[book][1]= "Unavailable"
-      print("Book has been borrowed")
-    else:
-      print("Book is not available ")
-elif(status=="Return"):
-    if(library[book][1] == "Unavailable"):
-        print("The Book has been returned")
-        library[book][1] = "Available"
-    else:
-       print("The Book was not borrowed")
+while True:
+    books = [" HarryPotter: Author: J.K.Rowling","PercyJackson: Author: RickRiordan","SherlockHolmes: Author: ArthurConanDoyle"]
+    print("Books: ",books)
 
-book = input("Enter Name of the book: ")
-status = input("Enter wether if you wanna Borrow / Return: ")
+    #Taking inputs
+    book = input("Enter Name of the book: ")
+    status = input("Enter wether if you wanna Borrow / Return: ")
 
-if(status == "Borrow"):
-    if(library[book][1] == "Available"):
-      library[book][1]= "Unavailable"
-      print("Book has been borrowed")
-    else:
-      print("Book is not available ")
-elif(status=="Return"):
-    if(library[book][1] == "Unavailable"):
-        print("The Book has been returned")
-        library[book][1] = "Available"
-    else:
-       print("The Book was not borrowed")
-print("Books status:",library)
+    #conditionals on basis of borrow or return
+    if(status == "Borrow"):
+        if(library[book][1] == "Available"):
+            library[book][1]= "Unavailable"
+            print("Book has been borrowed")
+        else:
+            print("Book is not available ")
+    elif(status=="Return"):
+        if(library[book][1] == "Unavailable"):
+            print("The Book has been returned")
+            library[book][1] = "Available"
+        else:
+            print("The Book was not borrowed")
+         # Loops Exit 
+    again = input("Want to Borrow or return? (yes/no): ").lower()
+    if again != "yes":
+        break
+
 
 
